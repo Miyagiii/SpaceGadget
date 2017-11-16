b7fb035d6ded071f5d17329d6584be2937c54c60
TheSpaceCowboy <deadpool42534@gmail.com>
2017-11-16 16:47:16 +0000

    Revamped music system
    Fixed tons of issues, with the music system.

---
+++ plugins/Beta-Music Legacy system.py
@@ -0,0 +1,214 @@
+#
+#   Currently in development, buggy as hell and not commented yet, i do not recommend using this module
+#
+#
+
+
+
+import discord,youtube_dl,time,asyncio
+from discord.ext import commands
+from plugins.Helper import Helper
+
+class Music:
+    description = """Music"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice = None
+        self.player = None
+        self.isPlaying = False
+        self.queue = []
+        self.sVotes = []
+        
+        print('Addon "{}" loaded'.format(self.__class__.__name__))
+
+    @commands.command(pass_context = True)
+    async def q(self,ctx):
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(ctx.message.content == "" or ctx.message.content == " " or ctx.message.content =="s.q"):     
+                await self.bot.say("You actually need to search for something ya dingus")
+                return
+            await self.bot.say(embed=discord.Embed(title="Added: "+ctx.message.content.replace("s.q ","")+" to queue",description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
+            self.queue.append(ctx.message.content.replace("s.q ",""))
+            if(len(self.queue) == 1):
+                await self.play(ctx)
+    @commands.command(pass_context = True)
+    async def skip(self,ctx):
+        if(len(self.queue) == 0):
+            await self.bot.say("No songs in queue to skip")
+        if(await Helper.isPerms(self,ctx,1)):
+            await self.bot.say("`Skip sucessful`")
+            self.sVotes = []
+            del self.queue[0]
+            self.player.stop()
+            if(len(self.queue) >0):
+                await self.bot.say("`Skipping to "+self.queue[0]+"`")
+                await self.play(ctx)
+            else:
+                return
+        if(await Helper.isBanned(self,ctx)):
+            
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(self.isPlaying == False):
+                await self.bot.say("Bot isn't playing anything")
+                return
+            if(self.voice == None):
+                await self.bot.say("Bot not in voice chat")
+                return
+            for items in self.sVotes:
+                if(ctx.message.author.id == items):
+                    await self.bot.say("Can't vote again")
+                    return
+            self.sVotes.append(ctx.message.author.id)
+            await self.bot.say("`Skip - "+str(len(self.sVotes))+"/"+str(int(len(self.voice.channel.voice_members) /2))+"`")
+            if(len(self.sVotes) >= (len(self.voice.channel.voice_members) /2)):
+                await self.bot.say("`Skip sucessful`")
+                self.sVotes = []
+                del self.queue[0]
+                self.player.stop()
+                if(len(self.queue) >0):
+                    await self.bot.say("`Skipping to "+self.queue[0]+"`")
+                    await self.play(ctx)
+                else:
+                    return
+            
+        
+    @commands.command(pass_context = True)
+    async def show_queue(self,ctx):
+        r = ""
+        x = 0
+        if(len(self.queue) == 0):
+            await self.bot.say(embed=discord.Embed(title="Nothing in queue!",description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
+            return
+            
+        for items in self.queue:
+            x+=1
+            r+="`#"+str(x)+":` "+items+" - "+ctx.message.author.name+"\n"
+        await self.bot.say(embed=discord.Embed(title="Queue",description=r,colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
+
+               
+    @commands.command(pass_context = True)
+    async def join_vc(self,ctx): 
+        """Joins a voice chat. Syntax: s.join_vc"""
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(self.isPlaying == True):
+                await self.bot.say("Can't move chat while playing music")
+                return
+            if(self.bot.voice_client_in(ctx.message.author.server) is not None):
+                self.voice = self.bot.voice_client_in(ctx.message.author.server)
+            channel = ctx.message.author.voice.voice_channel
+            if( ctx.message.author.voice.voice_channel != self.voice and self.voice is not None):
+                self.voice = await self.voice.disconnect()
+            if(ctx.message.author.voice.voice_channel is None):
+                await self.bot.say("```You have to be in a chat for the bot to join you, MORON!!!```")
+                return
+            if(self.voice is not None):
+                
+                self.voice = await self.voice.move_to(channel)
+                return
+            self.voice = await self.bot.join_voice_channel(channel)
+            await Helper.log(self,str(ctx.message.author),"joined vc "+channel.name,str(ctx.message.timestamp))
+
+    async def clears(self,ctx):
+        for x in range(0,len(self.queue)):
+            del self.queue[x]
+            self.isPlaying = False
+    @commands.command(pass_context = True)
+    async def clear(self,ctx):
+        if(await Helper.isBanned(self,ctx)):
+           return
+        if(await Helper.isPerms(self,ctx,1)):
+            if(self.player is not None):
+                self.player.stop()
+            await self.clears(ctx)
+        
+        
+
+    @commands.command(pass_context = True)
+    async def disconnect_vc(self,ctx):
+        """Disconnects bot from voice chat. Syntax: s.disconnect_vc"""
+        if(await Helper.isBanned(self,ctx)):
+           return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(self.isPlaying == True):
+                if(await Helper.isPerms(self,ctx,1)):
+                    await self.bot.say("Administrative disconnect Successful")
+                    await self.clears(ctx)
+                else:
+                    await self.bot.say("You cannot disconnect bot while it's playing music")
+                    return
+
+            if(self.bot.voice_client_in(ctx.message.author.server) is not None):
+                 self.voice = self.bot.voice_client_in(ctx.message.author.server) 
+                 await self.voice.disconnect()
+                 self.isPlaying = False
+            else:
+                await self.bot.say("Bot not in voice chat")
+                return
+            await Helper.log(self,str(ctx.message.author),"disconnected from vc",str(ctx.message.timestamp))
+
+    async def play(self,ctx):
+        """Play music. Syntax: s.play [url]"""
+        while(len(self.queue) != 0):
+            if(len(self.queue) == 0):
+                return
+            if(self.isPlaying == True):
+                self.player.stop()
+            if(self.voice is None and self.bot.voice_client_in(ctx.message.author.server) is not None):
+                self.voice = self.bot.voice_client_in(ctx.message.author.server)
+            elif(ctx.message.author.voice_channel is not None and self.voice is None):
+                self.voice = await self.bot.join_voice_channel(ctx.message.author.voice_channel)
+            else:
+                self.voice = self.bot.voice_client_in(ctx.message.author.server)
+            if(self.voice is None):
+                if(ctx.message.author.voice.voice_channel is not None ):
+                    self.voice = await self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+                else:                   
+                    await self.bot.say("Bot isn't in a voice channel")
+                    return
+            message = self.queue[0]#ctx.message.content.replace("s.play ","")
+            await self.bot.say(embed=discord.Embed(title="Now Playing: "+message,description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
+            beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+            ytdl_meta_opts = {
+                    'default_search': 'auto',
+                    'simulate': True,
+                    'quiet': True,
+            }
+            self.player = await self.voice.create_ytdl_player(message,ytdl_options=ytdl_meta_opts,before_options=beforeArgs)
+                
+            self.player.volume = 1
+            self.player.start()
+            self.isPlaying = True
+            await Helper.log(self,str(ctx.message.author),"played "+message,str(ctx.message.timestamp))
+            while self.player.is_playing():
+                pass
+            self.isPlaying = False
+
+            if(len(self.queue) > 1):
+                del self.queue[0]
+            self.player.stop()
+
+        await self.bot.say("No songs in queue  ¯\_(ツ)_/¯")
+        self.bot.isPlaying = False
+            
+    @commands.command(pass_context = True)
+    async def stop(self,ctx):
+        """Stops music. Syntax: s.stop"""
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,1)):
+            if(self.player is None):
+                await self.bot.say("No song playing!")
+            self.player.stop()
+            self.isPlaying = False
+            await self.clears(ctx)
+            await Helper.log(self,str(ctx.message.author),"Stop",str(ctx.message.timestamp))
+
+            
+def setup(bot):
+    bot.add_cog(Music(bot))
--- plugins/Beta-Music.py
+++ plugins/Beta-Music.py
@@ -2,210 +2,196 @@
 #   Currently in development, buggy as hell and not commented yet, i do not recommend using this module
 #
 #
-
-
-
-import discord,youtube_dl,time,asyncio
+import discord,youtube_dl,time,asyncio,datetime,locale
 from discord.ext import commands
 from plugins.Helper import Helper
 
-class Music:
-    description = """Music"""
-    
+class Player:
+
     def __init__(self, bot):
         self.bot = bot
-        self.voice = None
         self.player = None
-        self.isPlaying = False
+        self.skipVotes = []
+        if(len(list(self.bot.voice_clients)) <= 0):
+            self.voice = None
+        else:
+            self.voice = list(self.bot.voice_clients)[0]
         self.queue = []
-        self.sVotes = []
-        
-        print('Addon "{}" loaded'.format(self.__class__.__name__))
 
+    @commands.command(pass_context = True)
+    async def joinvc(self,ctx):
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(ctx.message.author.voice.voice_channel is None):
+                await self.bot.say("You must join a voice channel first to add the bot.")
+                return
+            if(self.player is not None):     
+                if(self.player.is_playing()):
+                    await self.bot.say("Cannot move bot while is playing.")
+                    return
+            if(self.bot.is_voice_connected(ctx.message.author.server) == False):
+                self.voice = await self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+            else:
+                self.voice = list(self.bot.voice_clients)[0]
+                self.voice = await self.voice.move_to(ctx.message.author.voice.voice_channel)
+                
+    @commands.command(pass_context = True)
+    async def disconnectvc(self,ctx):
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(self.bot.is_voice_connected(ctx.message.author.server) == False):                  
+                await self.bot.say("Bot not in a voice channel")
+                return
+        if(self.player is not None):     
+                if(self.player.is_playing()):
+                    await self.bot.say("Cannot move bot while is playing.")
+                    return
+
+        if(self.bot.is_voice_connected(ctx.message.author.server) and self.voice == None):
+            self.voice = list(self.bot.voice_clients)[0]
+        await self.voice.disconnect()
+    @commands.command(pass_context = True)
+    async def p(self,ctx):
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(len(self.queue) >= 25):
+                await self.bot.say(embed=discord.Embed(title="Notice:",description="Cannot add more than 25 tracks at a time",colour = 0x206694))
+                return
+            url = ctx.message.content.replace("s.p ", "")
+            if(self.voice == None and ctx.message.author.voice.voice_channel is not None):
+                if(self.bot.is_voice_connected(ctx.message.author.server) == False):
+                    self.voice = await self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+                else:
+                    self.voice = list(self.bot.voice_clients)[0]
+                    self.voice = await self.voice.move_to(ctx.message.author.voice.voice_channel)
+
+            if(self.voice == None):
+                await self.bot.say("Not in voice channel")
+                return
+            if(url == ""):
+                await self.bot.say("You need to search something")
+                return
+            
+            await self.retrieveSong(url,ctx.message.author)
     @commands.command(pass_context = True)
     async def q(self,ctx):
         if(await Helper.isBanned(self,ctx)):
             return
         if(await Helper.isPerms(self,ctx,3)):
-            if(ctx.message.content == "" or ctx.message.content == " " or ctx.message.content =="s.q"):     
-                await self.bot.say("You actually need to search for something ya dingus")
+            if(len(self.queue) <= 0):
+                await self.bot.say(embed=discord.Embed(title="Notice:",description="No tracks in queue",colour = 0x206694))
                 return
-            await self.bot.say(embed=discord.Embed(title="Added: "+ctx.message.content.replace("s.q ","")+" to queue",description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
-            self.queue.append(ctx.message.content.replace("s.q ",""))
-            if(len(self.queue) == 1):
-                await self.play(ctx)
-    @commands.command(pass_context = True)
-    async def skip(self,ctx):
-        if(len(self.queue) == 0):
-            await self.bot.say("No songs in queue  ¯\_(ツ)_/¯")
-        if(await Helper.isPerms(self,ctx,1)):
-            await self.bot.say("`Skip sucessful`")
-            self.sVotes = []
-            del self.queue[0]
-            self.player.stop()
-            if(len(self.queue) >0):
-                await self.bot.say("`Skipping to "+self.queue[0]+"`")
-                await self.play(ctx)
-            else:
-                return
-        if(await Helper.isBanned(self,ctx)):
+            embed = discord.Embed(title="Queue:",colour = 0x206694)
+            for x in range(0,len(self.queue)):
+                embed.add_field(name="---------------------------------------------------------------------------------------------------",
+                                value="**"+self.queue[x].name+"** : `"+str(self.queue[x].duration)+"` - **"+self.queue[x].requester.name+"**")
+            await self.bot.say(embed=embed)
             
-            return
-        if(await Helper.isPerms(self,ctx,3)):
-            if(self.isPlaying == False):
-                await self.bot.say("Bot isn't playing anything")
-                return
-            if(self.voice == None):
-                await self.bot.say("Bot not in voice chat")
-                return
-            for items in self.sVotes:
-                if(ctx.message.author.id == items):
-                    await self.bot.say("Can't vote again")
-                    return
-            self.sVotes.append(ctx.message.author.id)
-            await self.bot.say("`Skip - "+str(len(self.sVotes))+"/"+str(int(len(self.voice.channel.voice_members) /2))+"`")
-            if(len(self.sVotes) >= (len(self.voice.channel.voice_members) /2)):
-                await self.bot.say("`Skip sucessful`")
-                self.sVotes = []
+
+            
+    async def retrieveSong(self,url,requester):
+        ytdl_meta_opts = { 
+                        'default_search': 'auto',
+                        'simulate': True,
+                        'quiet': True,
+                }
+        try:
+            songRet = await self.voice.create_ytdl_player(url,ytdl_options=ytdl_meta_opts)
+        except Exception as e:
+            await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description="Something went wrong :/ "+str(e)))
+
+            
+        locale.setlocale(locale.LC_ALL, "en-GB")
+        self.queue.append(Song(songRet.title,datetime.timedelta(seconds=songRet.duration),songRet.uploader,locale.format("%d",songRet.views,grouping=True),locale.format("%d",songRet.dislikes,grouping=True),locale.format("%d",songRet.likes,grouping=True),songRet.upload_date,songRet.download_url,requester))
+        
+        embed = discord.Embed(title="Adding: "+self.queue[-1].name,colour = 0x206694,url=self.queue[-1].url)
+        embed.add_field(name="Duration:", value=self.queue[-1].duration)
+        embed.add_field(name="Uploader:", value=self.queue[-1].uploader)
+        embed.add_field(name="Views:", value=self.queue[-1].views)
+        embed.add_field(name="Likes:", value=self.queue[-1].likes)
+        embed.add_field(name="Dislikes:", value=self.queue[-1].dislikes)
+        embed.add_field(name="Upload Date:", value=self.queue[-1].uploadDate)
+        embed.set_author(name=self.queue[-1].requester.name, icon_url=requester.avatar_url)
+        await self.bot.say(embed=embed)
+        if(len(self.queue) <= 1):
+            await self.play()
+    async def play(self):
+        while len(self.queue) != 0:
+            try:
+                song = self.queue[0]
+                beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+                ytdl_meta_opts = { 
+                        'default_search': 'auto',
+                        'simulate': True,
+                        'quiet': True,
+                }
+                self.player = await self.voice.create_ytdl_player(song.url,ytdl_options=ytdl_meta_opts,before_options=beforeArgs)
+                self.player.start()
+                while self.player.is_playing():
+                    await asyncio.sleep(0.03)
+                self.player.stop()
                 del self.queue[0]
-                self.player.stop()
-                if(len(self.queue) >0):
-                    await self.bot.say("`Skipping to "+self.queue[0]+"`")
-                    await self.play(ctx)
-                else:
-                    return
-            
+            except Exception as e:
+                await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description="Something went wrong :/ "+e))
+        self.player.stop()
         
-    @commands.command(pass_context = True)
-    async def show_queue(self,ctx):
-        r = ""
-        x = 0
-        if(len(self.queue) == 0):
-            await self.bot.say(embed=discord.Embed(title="Nothing in queue!",description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
-            return
-            
-        for items in self.queue:
-            x+=1
-            r+="`#"+str(x)+":` "+items+" - "+ctx.message.author.name+"\n"
-        await self.bot.say(embed=discord.Embed(title="Queue",description=r,colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
+        await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description="queue empty¯\_(ツ)_/¯"))
 
-               
-    @commands.command(pass_context = True)
-    async def join_vc(self,ctx): 
-        """Joins a voice chat. Syntax: s.join_vc"""
-        if(await Helper.isBanned(self,ctx)):
-            return
-        if(await Helper.isPerms(self,ctx,3)):
-            if(self.isPlaying == True):
-                await self.bot.say("Can't move chat while playing music")
-                return
-            if(self.bot.voice_client_in(ctx.message.author.server) is not None):
-                self.voice = self.bot.voice_client_in(ctx.message.author.server)
-            channel = ctx.message.author.voice.voice_channel
-            if( ctx.message.author.voice.voice_channel != self.voice and self.voice is not None):
-                self.voice = await self.voice.disconnect()
-            if(ctx.message.author.voice.voice_channel is None):
-                await self.bot.say("```You have to be in a chat for the bot to join you, MORON!!!```")
-                return
-            if(self.voice is not None):
-                self.voice = await self.voice.move_to(channel)
-                return
-            self.voice = await self.bot.join_voice_channel(channel)
-            await Helper.log(self,str(ctx.message.author),"joined vc "+channel.name,str(ctx.message.timestamp))
-
-    async def clears(self,ctx):
-        for x in range(0,len(self.queue)):
-            del self.queue[x]
-            self.isPlaying = False
-    @commands.command(pass_context = True)
-    async def clear(self,ctx):
-        if(await Helper.isBanned(self,ctx)):
-           return
-        if(await Helper.isPerms(self,ctx,1)):
-            if(self.player is not None):
-                self.player.stop()
-            await self.clears(ctx)
-        
-        
-
-    @commands.command(pass_context = True)
-    async def disconnect_vc(self,ctx):
-        """Disconnects bot from voice chat. Syntax: s.disconnect_vc"""
-        if(await Helper.isBanned(self,ctx)):
-           return
-        if(await Helper.isPerms(self,ctx,3)):
-            if(self.isPlaying == True):
-                if(await Helper.isPerms(self,ctx,1)):
-                    await self.bot.say("Administrative disconnect Successful")
-                    await self.clears(ctx)
-                else:
-                    await self.bot.say("You cannot disconnect bot while it's playing music")
-                    return
-
-            if(self.bot.voice_client_in(ctx.message.author.server) is not None):
-                 self.voice = self.bot.voice_client_in(ctx.message.author.server) 
-                 await self.voice.disconnect()
-                 self.isPlaying = False
-            else:
-                await self.bot.say("Bot not in voice chat")
-                return
-            await Helper.log(self,str(ctx.message.author),"disconnected from vc",str(ctx.message.timestamp))
-
-    async def play(self,ctx):
-        """Play music. Syntax: s.play [url]"""
-        while(len(self.queue) != 0):
-            if(len(self.queue) == 0):
-                return
-            if(self.isPlaying == True):
-                self.player.stop()
-            if(self.voice is None and self.bot.voice_client_in(ctx.message.author.server) is not None):
-                self.voice = self.bot.voice_client_in(ctx.message.author.server)
-            elif(ctx.message.author.voice_channel is not None and self.voice is None):
-                self.voice = await self.bot.join_voice_channel(ctx.message.author.voice_channel)
-            else:
-                self.voice = self.bot.voice_client_in(ctx.message.author.server)
-            if(self.voice is None):
-                if(ctx.message.author.voice.voice_channel is not None ):
-                    self.voice = await self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
-                else:                   
-                    await self.bot.say("Bot isn't in a voice channel")
-                    return
-            message = self.queue[0]#ctx.message.content.replace("s.play ","")
-            await self.bot.say(embed=discord.Embed(title="Now Playing: "+message,description="",colour = 0x206694)) # Display's the synopsis with all relivent data in the embed
-            beforeArgs = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-            ytdl_meta_opts = {
-                    'default_search': 'auto',
-                    'simulate': True,
-                    'quiet': True,
-            }
-            self.player = await self.voice.create_ytdl_player(message,ytdl_options=ytdl_meta_opts,before_options=beforeArgs)
-                
-            self.player.volume = 1
-            self.player.start()
-            self.isPlaying = True
-            await Helper.log(self,str(ctx.message.author),"played "+message,str(ctx.message.timestamp))
-            await asyncio.sleep(self.player.duration+5)
-            if(self.isPlaying == False):
-                return
-            del self.queue[0]
-            self.player.stop()
-
-        await self.bot.say("No songs in queue  ¯\_(ツ)_/¯")
-        self.bot.isPlaying = False
-            
     @commands.command(pass_context = True)
     async def stop(self,ctx):
-        """Stops music. Syntax: s.stop"""
         if(await Helper.isBanned(self,ctx)):
             return
         if(await Helper.isPerms(self,ctx,1)):
-            if(self.player is None):
-                await self.bot.say("No song playing!")
-            self.player.stop()
-            self.isPlaying = False
-            await self.clears(ctx)
-            await Helper.log(self,str(ctx.message.author),"Stop",str(ctx.message.timestamp))
+            if(self.player is None or self.player.is_playing() == False):
+                await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description="Nothing is playing"))
 
+                return
+            self.player.stop()
+    @commands.command(pass_context = True)
+    async def skip(self,ctx): 
+        if(await Helper.isBanned(self,ctx)):
+            return
+        if(await Helper.isPerms(self,ctx,3)):
+            if(self.player is None or self.voice is None):
+                await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description=":no_entry_sign: **Nothing is playing** :no_entry_sign: "))
+                return
+            if(self.player.is_playing() == False):
+                await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description=":no_entry_sign: **Nothing is playing** :no_entry_sign: "))
+            if(await Helper.getRole(self,ctx,ctx.message.author.id) <= 1):
+                await self.doSkip()
+                return
+            if(len(self.skipVotes) >= 0):
+                for ids in self.skipVotes:
+                    if(ids == ctx.message.author.id):
+                        await self.bot.say("Can't vote twice")
+        
+                        return
+            self.skipVotes.append(ctx.message.author.id)
+            await self.bot.say(" Votes required to skip: `"+str(len(self.skipVotes))+"/"+str(int(len(self.voice.channel.voice_members) /2))+"`")
+            if(len(self.skipVotes) >= (len(self.voice.channel.voice_members) /2)):
+                await self.doSkip()
+                await self.bot.say(embed=discord.Embed(title="Notice:",colour = 0x206694,description=":fast_forward: **Skipping** :fast_forward: "))
+                self.skipVotes = []
             
+                
+                
+    async def doSkip(self):
+        self.player.stop()
+        
+            
+class Song:
+    def __init__(self,title,duration,uploader,views,dislikes,likes,uploadDate,url,requester):
+        self.name = title
+        self.duration = duration
+        self.uploader = uploader
+        self.views = views
+        self.dislikes = dislikes
+        self.likes = likes
+        self.uploadDate = uploadDate
+        self.url = url
+        self.requester = requester
 def setup(bot):
-    bot.add_cog(Music(bot))
+    bot.add_cog(Player(bot))
